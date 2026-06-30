a555becc61f700f8ddd41cec37d64d2aa40554dc
Mylord1406 <mylordbokemba6@gmail.com>
2026-06-30 21:42:50 +0100

    debug: test with simple http server

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "=== DEBUT DU SCRIPT ===" 1>&2
+echo "=== DEBUT ===" 1>&2
 python manage.py migrate --settings=config.settings.production 1>&2
-echo "=== FIN MIGRATE ===" 1>&2
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --log-level debug 1>&2
+echo "=== MIGRATE OK ===" 1>&2
+echo "=== PORT EST: $PORT ===" 1>&2
+python -m http.server $PORT 1>&2
